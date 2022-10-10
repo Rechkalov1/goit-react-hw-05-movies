9c4d59163fcbe17f98268971b91065d8f90ecf5f
EnVy <envy7321@gmail.com>
2022-10-10 14:02:50 +0300

    save

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
-import { BrowserRuoter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <BrowserRuoter>
+  <BrowserRouter>
     <App />
-  </BrowserRuoter>
+  </BrowserRouter>
   // </React.StrictMode>
 );
