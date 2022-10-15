c5ea8c2787f7f3e6255a0abee85147ba48fbd390
Rechkalov1 <97546360+Rechkalov1@users.noreply.github.com>
2022-10-15 14:44:14 +0000

    Deploying to gh-pages from @ Rechkalov1/goit-react-hw-05-movies@39834763983dcf04c20c91f23d9cdc03857cc8eb 🚀

---
+++ static/js/794.1c5b2ccc.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[794],{794:function(e,r,n){n.r(r),n.d(r,{default:function(){return t}});var i=n(731),s=n(184);function t(){return(0,s.jsxs)("div",{children:[(0,s.jsx)("h1",{children:"OOPS the page you were looking for is "}),(0,s.jsx)("h2",{children:"Maybe those links can help you "}),(0,s.jsx)("ul",{children:(0,s.jsx)("li",{children:(0,s.jsx)(i.rU,{to:"/movies",children:" Movies"})})})]})}}}]);
+//# sourceMappingURL=794.1c5b2ccc.chunk.js.map
